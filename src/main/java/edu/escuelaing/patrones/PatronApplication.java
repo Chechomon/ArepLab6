9ed394875259742e89fe666dae859195021cd899
Sergio Peña <alejope98@hotmail.com>
2020-03-19 19:49:57 -0500

    application

---
+++ src/main/java/edu/escuelaing/patrones/PatronApplication.java
@@ -0,0 +1,15 @@
+package edu.escuelaing.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @author alejo
+ */
+@SpringBootApplication
+public class PatronApplication {
+     public static void main(String[] args) {
+		SpringApplication.run(PatronApplication.class, args);
+	}
+ 
+}
